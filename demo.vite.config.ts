040f9c4abd78084e288249bc7645508865f49761
melvi-l <melvil.miglio@gmail.com>
2025-07-12 18:08:11 +0200

    chore: fix demo

--- demo.vite.config.ts
+++ demo.vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "demo",
-  base: "/",
+  base: "/color-picker-webgpu",
   build: {
     outDir: "../gh-pages",
     emptyOutDir: true,
